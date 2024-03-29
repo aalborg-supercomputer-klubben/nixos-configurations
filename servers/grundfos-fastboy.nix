1b8ba82f93b404b72e98aee934fd673f26e0acbd
AalborgSupercomputerKlub <aalborgsupercomputerklub@gmail.com>
2024-03-29 13:53:07 +0100

    add configuration for fastboy

---
+++ servers/grundfos-fastboy.nix
@@ -0,0 +1,23 @@
+# Configuration file for the grundfos-fastboy server
+# Specs:
+# 32 cores
+# 128 GB ram
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ../common.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "grundfos-fastboy";
+
+  networking.useDHCP = false;
+  networking.interfaces.eno1.useDHCP = true;
+}
+

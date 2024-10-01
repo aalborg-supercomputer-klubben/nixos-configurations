dde2f373d92dc91940f31fb4951b3a11159cf68d
Thomas <mast3r@skade.dev>
2024-10-01 17:07:30 +0200

    add last ssh key

--- common.nix
+++ common.nix
@@ -27,6 +27,7 @@
     ssh-keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILHWivy/fWfI1RnUAKLC4azHaydg2EC4JCvvDJs2/d90 tobias@nixos"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG5Lc6TJ+9DmxiSmUBb08glscp3aL4Xm0v0j2jVlZTUX mast3r@laptop"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOyNgY9a3t7ZWT9MnM3ePe5m+MNIvsoZWo7OqU7JsPqn caspernyvang@gmail.com"
     ];
   {
     asck = {

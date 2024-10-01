783a3818f02a38816ced43aa3198f35e3f16c9c4
Thomas <mast3r@skade.dev>
2024-10-01 16:43:58 +0200

    added experimental features

--- common.nix
+++ common.nix
@@ -79,6 +79,11 @@
     users.users.root.password = "1234";
   };
 
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.

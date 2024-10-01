d83744c5ee7b566a92c144c7ae3e95aa3335d18a
Thomas <mast3r@skade.dev>
2024-10-01 16:24:06 +0200

    remove system from nixosconfigurations

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
   in rec {
-    nixosConfigurations.${system} = {
+    nixosConfigurations = {
       grundfos-fastboy = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [

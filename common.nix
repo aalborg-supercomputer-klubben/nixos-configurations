b077451e516cbfe4a5b1f0d1c5d3796bb0358d9f
Thomas <mast3r@skade.dev>
2024-10-02 09:29:41 +0200

    rewrote it to just have a user per member

--- common.nix
+++ common.nix
@@ -3,6 +3,9 @@
 { config, lib, pkgs, ... }:
 
 {
+  imports = [
+    ./users.nix
+  ];
 
   # Set your time zone.
   time.timeZone = "Europe/Copenhagen";
@@ -15,19 +18,22 @@
   i18n.defaultLocale = "en_US.UTF-8";
   console.keyMap = "dk";
 
-  users.users = let
-    ssh-keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILHWivy/fWfI1RnUAKLC4azHaydg2EC4JCvvDJs2/d90 tobias@nixos"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG5Lc6TJ+9DmxiSmUBb08glscp3aL4Xm0v0j2jVlZTUX mast3r@laptop"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOyNgY9a3t7ZWT9MnM3ePe5m+MNIvsoZWo7OqU7JsPqn caspernyvang@gmail.com"
-    ];
-  in {
-    asck = {
+  users.users = {
+    mast3r = {
       isNormalUser = true;
-      description = "default user";
-      openssh.authorizedKeys.keys = ssh-keys;
+      description = "https://github.com/Mast3rwaf1z";
+      openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG5Lc6TJ+9DmxiSmUBb08glscp3aL4Xm0v0j2jVlZTUX mast3r@laptop"];
     };
-    root.openssh.authorizedKeys.keys = ssh-keys;
+    tobias = {
+      isNormalUser = true;
+      description = "";
+      openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILHWivy/fWfI1RnUAKLC4azHaydg2EC4JCvvDJs2/d90 tobias@nixos"];
+    };
+    casper = {
+      isNormalUser = true;
+      description = "";
+      openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOyNgY9a3t7ZWT9MnM3ePe5m+MNIvsoZWo7OqU7JsPqn caspernyvang@gmail.com"];
+    };
   };
 
   # Programs and packages

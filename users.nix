4a95f98e12d1b2c78a3aaaeb69edb17e74a935a9
Mads Mogensen <mail@madsmogensen.dk>
2025-03-05 14:30:44 +0000

    Add me to users

--- users.nix
+++ users.nix
@@ -33,7 +33,7 @@ in {
     mast3r = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG5Lc6TJ+9DmxiSmUBb08glscp3aL4Xm0v0j2jVlZTUX mast3r@laptop"];
     tobias = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILHWivy/fWfI1RnUAKLC4azHaydg2EC4JCvvDJs2/d90 tobias@nixos"];
     casper = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOyNgY9a3t7ZWT9MnM3ePe5m+MNIvsoZWo7OqU7JsPqn caspernyvang@gmail.com"];
-    mads = [];
+    mads = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCTefeRmo6GME/t0C72h1o5VWcqYHKbl4aiR60A6k+zbEnqObUxHg1yj5b3+AeJRQjcYG//nwZhfgnLqPBASGUTyjn63CmWURx1+8ZDZN6uIhif/s+PEwjU/uuXW+2inEr+zGpRJZ75YuxWuL4L8g/yXj5HB8PbIuYpWwDq+UQOuOS8K52mC035oKZakCzR1GiJQJe4kUokDbhrNeYwW6p/K6wbFqNMQSJs1gkZ+S4NBXWEEtVaPwCpZ4K5fBOkywCLkMU6UZFvYS/Fev/RayDFvdW9e5yulwlvJZyTavTM0YYqd7QUAfTRIzR46tDXuRm1bms1ZqGaDj2NolgPwCAvMjFZPt5CAfqU7eaRfmDKzgoqgq00vrC1gaEXnboMziTqMvugejpagPP0s/UAZD79fdXsHPdh6sKlsNPyrWqQdb8ksHgv4qT2Vg2hzXiENiLBbzIK6aLzu6BqwQXKP/7+wdvORR+1YtW+L/2E0ULEwcTqJWPsxBs3jCuBkXYu5RMmJZEUcVz1kZh2G/6/ZAzu/FxpVXkAYLfnSHa8oAIpny9D64/Qwfo2cTKmSc1D/crhtWsP3eBz8tACQO1AsQOqeq0iMqfY5buUSSGr4QxPpqPtDxNGrO2peCTnCHuUSX+Cge11+doln061JR236HOoxxNCbkc7QWGNQsqxSJxB3Q== mads@laptop-mads"];
     sofie = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJvgn0kSAboULv37yLS1fGwByGSudhbQGrP/RrO7+cH+"];
   };
 }

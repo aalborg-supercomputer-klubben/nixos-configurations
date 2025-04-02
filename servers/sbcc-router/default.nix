52ecab89bfe677c71698db2ddd02633b3b98b3ca
AshesOfEther <15670490+AshesOfEther@users.noreply.github.com>
2025-04-02 13:37:51 +0200

    feat: add sbcc-router config

---
+++ servers/sbcc-router/default.nix
@@ -0,0 +1,42 @@
+{ pkgs, ... }: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
+    initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+  };
+
+  networking.hostName = "sbcc-router";
+  networking.interfaces.end0.ipv4.addresses = [
+    {
+      address = "10.0.0.1";
+      prefixLength = 8;
+    }
+  ];
+
+  hardware.enableRedistributableFirmware = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
